90ab53c0532a1168b1cb43d85660d27a5fc31858
Márcio Winicius <marciowinicius.mw@gmail.com>
2018-06-12 12:12:58 -0300

    Alterado config.

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'winiciusamaral');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'products');
 
 /* Attempt to connect to MySQL database */
